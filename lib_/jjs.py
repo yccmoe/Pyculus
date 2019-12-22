2ccda466481596aca368bb49ac486bdba251ac21
얌츤츤 <yccmoe@gmail.com>
2019-12-22 22:27:40 +0900

    jjs
    전정실 길드+한글패치수정

--- lib_/jjs.py
+++ lib_/jjs.py
@@ -36,15 +36,14 @@ async def wow(bot,letter):
         #print(m[i],m[i+int((len(m)/2))])
         if m[i] =='120':
             #print qoute(m[i+int((len(m)/2))])
-            url='https://raider.io/api/v1/characters/profile?region=kr&realm=' + quote(m[i+int((len(m)/2))]) +'&name='+ q +'&fields=gear%2Cmythic_plus_weekly_highest_level_runs%2Cmythic_plus_best_runs'
+            url='https://raider.io/api/v1/characters/profile?region=kr&realm=' + quote(m[i+int((len(m)/2))]) +'&name='+ q +'&fields=gear%2Cmythic_plus_weekly_highest_level_runs%2Cmythic_plus_best_runs%2Cguild'
             #print(url)
             #print(m[i+int((len(m)/2))])
             r=requests.get(url).json()
-            head = r['name']+'@'+tr(m[i+int((len(m)/2))])+'\n'+str(r['gear']['item_level_equipped'])+' '+tr(r['active_spec_name'])+'\n - - - -\n'
+            try: head = r['name']+'@'+tr(m[i+int((len(m)/2))])+'\n<'+r['guild']['name']+'>\n'+str(r['gear']['item_level_equipped'])+' '+tr(r['active_spec_name']+' '+r['class'])+'\n - - - -\n'
+            except: head = r['name']+'@'+tr(m[i+int((len(m)/2))])+'\n'+str(r['gear']['item_level_equipped'])+' '+tr(r['active_spec_name']+' '+r['class'])+'\n - - - -\n'
             brun = r['mythic_plus_best_runs']
             rrun = r['mythic_plus_weekly_highest_level_runs']
-            #print(brun)
-            #print(rrun)
 
             #for run in rrun:
             #    print(tr(run['short_name']),str(run['mythic_level']),tr(run['num_keystone_upgrades']))
@@ -66,7 +65,7 @@ def test():
         print(m[i],m[i+int((len(m)/2))])
         
 def tr(req):
-    list = {'Protection': '전탱', 'Arms': '무전', 'Fury': '분전', 'Beast Mastery': '야냥', 'Marksmanship': '격냥', 'Survival': '생냥', 'Elemental': '정술', 'Restoration': '복술', 'Enhancement': '고술', 'Brewmaster': '양조', 'Windwalker': '공옾', 'Mistweaver': '운무', 'Assassination': '암살', 'Outlaw': '무법', 'Subtlety': '잠행', 'Blood': '혈죽', 'Frost': '냉죽', 'Unholy': '부죽', 'Fire': '화법', 'Frost2': '냉법', 'Arcane': '비법', 'Guardian': '야탱', 'Feral': '야딜', 'Balance': '부엉', 'Restoration2': '회드', 'Protection2': '보기', 'Retribution': '징기', 'Holy2': '신기', 'Shadow': '암사', 'Discipline': '수사', 'Holy': '신사', 'Affliction': '고흑', 'Demonology': '악흑', 'Destruction': '파흑', 'Vengeance': '악탱', 'Havoc': '악딜', 'VOTW': '금고', 'BRH': '검때', 'NL': '넬타', 'COS': '별궁', 'ARC':'비전', 'EOA': '아즈', 'DHT': '어숲', 'MOS': '아귀', 'HOV': '용맹', 'UPPR': '상층', 'LOWR': '하층', 'COEN': '성당', 'SEAT': '삼두', 'AD': '아탈', 'FH': '자지', 'TD': '톨다', 'TOS': '세스', 'UNDR': '썩굴', 'SIEGE': '보랄', 'SOTS': '폭풍', 'KR': '왕안', 'WM': '저택', 'ML': '광산', 0: ' -소진- ' , 1:' ★' , 2:' ★★', 3:' ★★★','가로나':'가로나', '굴단':'굴단', '노르간논':'노르', '달라란':'달라란', '데스윙':'데스윙', '듀로탄':'듀로', '렉사르':'렉사', '말퓨리온':'말퓨', '불타는 군단':'불군', '세나리우스':'세나', '스톰레이지':'스톰', '아즈샤라':'아즈', '알렉스트라자':'알렉', '와일드해머':'해머', '윈드러너':'윈드', '줄진':'줄진', '하이잘':'하이잘', '헬스크림':'헬스'}
+    list = {'Protection Warrior': '전탱', 'ArmsWarrior': '무전', 'Fury Warrior': '분전', 'Beast Mastery Hunter': '야냥', 'Marksmanship Hunter': '격냥', 'Survival Hunter': '생냥', 'Elemental Shaman': '정술', 'Restoration Shaman': '복술', 'Enhancement Shaman': '고술', 'Brewmaster Monk': '양조', 'Windwalker Monk': '공옾', 'Mistweaver Monk': '운무', 'Assassination Rogue': '암살', 'Outlaw Rogue': '무법', 'Subtlety Rogue': '잠행', 'Blood Death Knight': '혈죽', 'Frost Death Knight': '냉죽', 'Unholy Death Knight': '부죽', 'Fire Mage': '화법', 'Frost Mage': '냉법', 'Arcane Mage': '비법', 'Guardian Druid': '야탱', 'Feral Druid': '야딜', 'Balance Druid': '부엉', 'Restoration Druid': '회드', 'Protection Paladin': '보기', 'Retribution Paladin': '징기', 'Holy Paladin': '신기', 'Shadow Priest': '암사', 'Discipline Priest': '수사', 'Holy Priest': '신사', 'Affliction Warlock': '고흑', 'Demonology Warlock': '악흑', 'Destruction Warlock': '파흑', 'Vengeance Demon Hunter': '악탱', 'Havoc Demon Hunter': '악딜', 'VOTW': '금고', 'BRH': '검때', 'NL': '넬타', 'COS': '별궁', 'ARC':'비전', 'EOA': '아즈', 'DHT': '어숲', 'MOS': '아귀', 'HOV': '용맹', 'UPPR': '상층', 'LOWR': '하층', 'COEN': '성당', 'SEAT': '삼두', 'AD': '아탈', 'FH': '자지', 'TD': '톨다', 'TOS': '세스', 'UNDR': '썩굴', 'SIEGE': '보랄', 'SOTS': '폭풍', 'KR': '왕안', 'WM': '저택', 'ML': '광산', 0: ' -소진- ' , 1:' ★' , 2:' ★★', 3:' ★★★','가로나':'가로나', '굴단':'굴단', '노르간논':'노르', '달라란':'달라란', '데스윙':'데스윙', '듀로탄':'듀로', '렉사르':'렉사', '말퓨리온':'말퓨', '불타는 군단':'불군', '세나리우스':'세나', '스톰레이지':'스톰', '아즈샤라':'아즈', '알렉스트라자':'알렉', '와일드해머':'해머', '윈드러너':'윈드', '줄진':'줄진', '하이잘':'하이잘', '헬스크림':'헬스'}
     try: return list.get(req)
     except: return req
 
